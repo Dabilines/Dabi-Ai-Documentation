0b23fa1b7a17b78edb05b193501e34cacd60ab68
Dabi <erkipandial@gmail.com>
2026-09-14 20:42:58 +0700

    Update func.js

--- assets/func.js
+++ assets/func.js
@@ -67,7 +67,7 @@ const sifatList = [ 'Baik', 'Jahat', 'Lucu', 'Pemarah', 'Penyabar', 'Pemalu', 'P
         60: { soal: "wajah yang _ _ _ _ _ _ _", jawaban: "menawan" },
       },
 
-      randTxt = ['haha luxu', 'y', 'asik sendiri', 'follow dong https://whatsapp.com/channel/0029Van8WHGEAKW8OUDniG1m', 'lu asik bang', 'disirkelin jir', 'when yah masuk sirkel']
+      randTxt = ['haha luxu', 'y', 'asik sendiri', 'follow dong https://whatsapp.com/channel/0029Van8WHGEAKW8OUDniG1m', 'lu asik bang', 'disirkelin jir', 'when yah masuk sirkel', 'haha kena sirkel', 'yah dicuekin']
 
 
 export default {
